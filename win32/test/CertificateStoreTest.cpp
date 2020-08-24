6a61a34df0e307a5fa1c5b5cfee70a566ad6694a
ddt-tdd <david.tillemans@cryptable.org>
2020-08-24 16:00:23 +0200

    Cleanup tests

--- win32/test/CertificateStoreTest.cpp
+++ win32/test/CertificateStoreTest.cpp
@@ -203,91 +203,76 @@ TEST_CASE( "CertificateStoreTests", "[success]" ) {
 TEST_CASE( "Key enforcement test with imported key", "[ui]" ) {
 
     // Arrange
-    CertStoreUtil certStoreUtil;
-    if (certStoreUtil.hasCertificates(L"John Doe")) {
-        certStoreUtil.deleteCertificates(L"John Doe");
+    SECTION("Import key with UI")
+    {
+        CertStoreUtil certStoreUtil;
+        if (certStoreUtil.hasCertificates(L"John Doe")) {
+            certStoreUtil.deleteCertificates(L"John Doe");
+        }
+        certStoreUtil.close();
+        OpenSSLCertificateRequest openSslCertificateRequest(std::string("/CN=John Doe/O=Company/C=US"), 2048);
+        OpenSSLCA openSslca("/CN=RootCA/O=Company/C=US", 4096);
+        auto cert = openSslca.certify(openSslCertificateRequest);
+        OpenSSLPKCS12 openSslpkcs12(*(cert.get()),
+                                    openSslca.getCertificate(),
+                                    openSslCertificateRequest.getKeyPair(),
+                                    std::string("system"));
+        auto pkcs12 = openSslpkcs12.getPKCS12();
+        auto b64pkcs12 = Base64Utils::toBase64(pkcs12);
+
+        //Act
+        CertificateStore certificateStore;
+        certificateStore.forcePasswordPINProtection(CertificateStore::strongKeyProtection::UIAlways);
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+        REQUIRE_NOTHROW(certificateStore.pfxImport(b64pkcs12, converter.from_bytes("system"), true));
     }
-    certStoreUtil.close();
-    OpenSSLCertificateRequest openSslCertificateRequest(std::string("/CN=John Doe/O=Company/C=US"), 2048);
-    OpenSSLCA openSslca("/CN=RootCA/O=Company/C=US", 4096);
-    auto cert = openSslca.certify(openSslCertificateRequest);
-    OpenSSLPKCS12 openSslpkcs12(*(cert.get()),
-                                openSslca.getCertificate(),
-                                openSslCertificateRequest.getKeyPair(),
-                                std::string("system"));
-    auto pkcs12 = openSslpkcs12.getPKCS12();
-    auto b64pkcs12 = Base64Utils::toBase64(pkcs12);
 
-    //Act
-    CertificateStore certificateStore;
-    certificateStore.forcePasswordPINProtection(CertificateStore::strongKeyProtection::UIAlways);
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-    REQUIRE_NOTHROW(certificateStore.pfxImport(b64pkcs12, converter.from_bytes("system"), true));
-
-    certStoreUtil.reopen();
-    REQUIRE(certStoreUtil.hasCertificates(L"John Doe"));
-    REQUIRE(certStoreUtil.hasPrivateKey(L"John Doe"));
+    SECTION("Signature test with UI")
+    {
+        CertStoreUtil certStoreUtil;
+        REQUIRE(certStoreUtil.hasCertificates(L"John Doe"));
+        REQUIRE(certStoreUtil.hasPrivateKey(L"John Doe"));
+    }
 }
 
 TEST_CASE( "Key enforcement test with generated key", "[ui]" ) {
     // Arrange
-    CertStoreUtil certStoreUtil;
-    if (certStoreUtil.hasCertificates(L"John Doe")) {
-        certStoreUtil.deleteCertificates(L"John Doe");
+    SECTION("Import key with UI") {
+        CertStoreUtil certStoreUtil;
+        if (certStoreUtil.hasCertificates(L"John Doe")) {
+            certStoreUtil.deleteCertificates(L"John Doe");
+        }
+        certStoreUtil.close();
+        CertificateStore certificateStore;
+        certificateStore.forcePasswordPINProtection(CertificateStore::strongKeyProtection::UIAlways);
+        auto csr = certificateStore.createCertificateRequest(std::string("cn=John Doe, o=Company, c=US"),
+                                                             2048,
+                                                             true);
+        OpenSSLCertificateRequest openSslCertificateRequest(csr);
+        OpenSSLCA openSslca("/CN=rootCA", 2048);
+        auto cert = openSslca.certify(openSslCertificateRequest);
+
+        // Act
+        REQUIRE_NOTHROW(certificateStore.importCertificate(cert->getPEM()));
     }
-    certStoreUtil.close();
-    CertificateStore certificateStore;
-    certificateStore.forcePasswordPINProtection(CertificateStore::strongKeyProtection::UIAlways);
-    auto csr = certificateStore.createCertificateRequest(std::string("cn=John Doe, o=Company, c=US"),
-                                                         2048,
-                                                         true);
-    OpenSSLCertificateRequest openSslCertificateRequest(csr);
-    OpenSSLCA openSslca("/CN=rootCA", 2048);
-    auto cert = openSslca.certify(openSslCertificateRequest);
 
-    // Act
-    REQUIRE_NOTHROW(certificateStore.importCertificate(cert->getPEM()));
-
-    // Assert
-    certStoreUtil.reopen();
+    SECTION("Verify Test Signature with UI") {
+        // Assert
+        CertStoreUtil certStoreUtil;
 #pragma warning(disable:4996) // DISABLE(has no impact) warning C4996: 'getenv': This function or variable may be unsafe. Consider using _dupenv_s instead.
-    if (getenv("SHOW_INFO") != NULL) {
+        if (getenv("SHOW_INFO") != NULL) {
 #pragma warning(default:4996)
-        certStoreUtil.showCertificatesOfCertStore();
-        KeyStoreUtil keyStoreUtil(MS_KEY_STORAGE_PROVIDER);
-        keyStoreUtil.showKeysOfKeystore();
-        std::cout << "David :" << std::endl;
-        certStoreUtil.showPropertiesOfCertificate(L"david");
+            certStoreUtil.showCertificatesOfCertStore();
+            KeyStoreUtil keyStoreUtil(MS_KEY_STORAGE_PROVIDER);
+            keyStoreUtil.showKeysOfKeystore();
+            std::cout << "David :" << std::endl;
+            certStoreUtil.showPropertiesOfCertificate(L"david");
+        }
+
+        REQUIRE(certStoreUtil.hasCertificates(L"John Doe"));
+        REQUIRE(certStoreUtil.hasPrivateKey(L"John Doe"));
+
+        // Cleanup
+        // certStoreUtil.deleteCertificates(L"John Doe");
     }
-
-    REQUIRE(certStoreUtil.hasCertificates(L"John Doe"));
-    REQUIRE(certStoreUtil.hasPrivateKey(L"John Doe"));
-
-    // Cleanup
-    // certStoreUtil.deleteCertificates(L"John Doe");
-}
-
-TEST_CASE( "Sign using CryptoAPI", "[ui]" ) {
-    CertStoreUtil certStoreUtil;
-
-    REQUIRE(certStoreUtil.hasPrivateKey(L"John Doe"));
-
-}
-
-TEST_CASE( "Export using CryptoAPI", "[ui]" ) {
-
-    // Act
-    std::string p12;
-    CertificateStore certificateStore;
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-    REQUIRE_NOTHROW(p12 = certificateStore.pfxExport(std::string("cn=rootCA"), std::string("03"),
-                                                     converter.from_bytes("system")));
-
-    // Assert
-    auto p12Data = Base64Utils::fromBase64(p12);
-    OpenSSLPKCS12 openSslpkcs12(p12Data.data(), p12Data.size(), "system");
-    REQUIRE(openSslpkcs12.getCertificate().getCommonName() == "John Doe");
-    REQUIRE(openSslpkcs12.getCAs().size() == 0);
-    REQUIRE(openSslpkcs12.getPrivateKey().getKeyBitlength() == 2048);
-
 }
